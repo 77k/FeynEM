27ca9ba2f02eb36ab83779696a5287d1381a2644
SUPER N00N <sn00n@77k.eu>
2017-08-04 15:06:37 +0200

    QED

--- qed.h
+++ qed.h
@@ -16,7 +16,6 @@ template <int Q,  int D, template< int _D > class M >
 struct YeeFieldSpace: public MortonSpace< Q, D, M >
 {
 
-//	typedef M< Q > Metric;
 	typedef SimpleEuklidianMetricFdouble< Q > Metric;
 	typedef typename M< Q >::KeyType KeyType;
 	typedef typename M< Q >::Point PointT;
@@ -62,7 +61,6 @@ struct YeeFieldSpace: public MortonSpace< Q, D, M >
 	friend std::ostream& operator << (std::ostream& s, YeeFieldSpace sp) 
 	{
 		PointT p = M< Q >::morton_decode(sp.v);
-//		for(int i = 0; i < D - 1; i++) s << p[i] << ", ";
 		s << "p = " << p << std::endl;;
 		s << "E = " <<  sp.E << std::endl; 
 		s << std::endl << "H = " << sp.H;
@@ -70,7 +68,6 @@ struct YeeFieldSpace: public MortonSpace< Q, D, M >
 	}
 
 
-//template< int X, int Y, typename Z >
 //friend        std::ostream& operator << (std::ostream& s, typename ExteriorPower< X, Y, Z >::Vector &p);
 
 };
@@ -136,7 +133,6 @@ class YeeSpace: public CAT< 0, D, _M>
 	//todo: division of Key into SubKeys
 	//test for chunk size cache compatible
 	std::vector< std::map< uint16_t, std::pair< Cube, ptrdiff_t > > > m_cube_tree;
-//	std::vector< std::map< uint16_t, std::pair< Cube, shared_pointer< > > > m_cube_tree;
         YeeSpace()
         {
          
@@ -195,7 +191,6 @@ class YeeSpace: public CAT< 0, D, _M>
 				
 			}
 
-			//std::cout << typeid(m_cube_tree[0]).name() << std::endl;	
 			m_cube_tree[0].find(subk) == end(m_cube_tree[0]) ? std::cout << "penis" : std::cout << "mafia";
 		}
 
@@ -239,21 +234,6 @@ class YeeSpace: public CAT< 0, D, _M>
 	void integrate(auto s_beg, auto s_end)
 	{
 		auto start = clock();
-		 //std::cout << "integrate bitch" << std::endl;
-	//	std::for_each(begin(mc_map), end(mc_map), [i = 0] (auto &p) mutable {
-
-//				typename Metric::Point p1 = Metric::morton_decode(p.first);
-			/*	std::cout << p1 << std::endl;
-				p1 = {1, 0, 0, 0};
-				// p = {t, x, y, z}
-				std::cout << "p1 = " << p1 << " " << std::bitset<std::numeric_limits< KeyType >::digits>(Metric::morton_encode(p1)) << std::endl;
-				p1 = {0, 1, 0, 0};
-				std::cout << "p1 = " << p1 << " " << std::bitset<std::numeric_limits< KeyType >::digits>(Metric::morton_encode(p1)) << std::endl;
-				p1 = {0, 0, 1, 0};
-				std::cout << "p1 = " << p1 << " " << std::bitset<std::numeric_limits< KeyType >::digits>(Metric::morton_encode(p1)) << std::endl;
-				p1 = {0, 0, 0, 1};
-				std::cout << "p1 = " << p1 << " " << std::bitset<std::numeric_limits< KeyType >::digits>(Metric::morton_encode(p1)) << std::endl;
-*/
 		std::vector<Cube> l_cubes;
 		for(auto iter = begin(mc_map); iter != end(mc_map); iter++)
 		{
@@ -265,9 +245,7 @@ class YeeSpace: public CAT< 0, D, _M>
 				double delta_x, delta_y, delta_z, delta_t;
 				typename Cube::Space::Vector delta({1.0, 1.0, 1.0, 1.0});
 				#define m(a) Metric::morton_encode(a)
-		//		std::cout << std::bitset<std::numeric_limits< KeyType >::digits>(iter->first) << std::endl;
 				P p0 = Metric::morton_decode(iter->first);
-			//	K k1 = m(p0 + P({1,1,1,0}));
 				for(int i = 0; i < 27; i++)
 				{
 					P q;
@@ -277,7 +255,6 @@ class YeeSpace: public CAT< 0, D, _M>
 					P p_1_1_1_0, p_1_1__1_0;
 					Cube C_1_1_1_0 = getCube(p0 + P({1,1,1,0}));
 					Cube C_1_1_m1_0 = getCube(p0 + P({1,1,-1,0}));
-					//std::cout << "p_n2 = " << p_n2 << std::endl;
 					Cube C_2_i_j_k = getCube(p_n2);
 					C_2_i_j_k.E[0] =
 						2.0 * delta[0]/(2.0 * getCube(m(p0)).epsilon + getCube(m(p0)).sigma * delta[0])				
@@ -300,17 +277,13 @@ class YeeSpace: public CAT< 0, D, _M>
 				}
 
 
-//				std::cout << std::bitset<std::numeric_limits< KeyType >::digits>(m(p0 + P({1,1,-1,0}))) << std::endl;
 		}
 		for(auto iter = begin(l_cubes); iter != end(l_cubes); iter++)
 		{
 
 			
 			insert_mc_map(*iter);
-			//mc_map.insert(make_pair(iter->v, *iter));
 		}
-		std::cout << "thread id = " << std::this_thread::get_id() << "mc_map.size() = " << mc_map.size() 
-		<< "integration time: " << ((double)(clock() - start))/CLOCKS_PER_SEC << std::endl;		
 
 								
 								
@@ -318,49 +291,4 @@ class YeeSpace: public CAT< 0, D, _M>
 
 };
 
-/*
-
-template< int D, template< int _D > class M >
-class YeeSpace: public CubicalSpaceCompressed< D, M>
-{
-	public:
-
-        enum { d = D};
-        typedef M< D > Metric;
-        typedef HyperCubeTree< D, 0, M, std::vector, std::allocator > Tree;
-        typedef typename Metric::KeyType KeyType;
-        typedef typename Metric::ValueType ValueType;
-        typedef typename Metric::PValueType PValueType;
-        typedef typename Metric::template ElementT< d > PointT;
-        typedef typename ExteriorPower< 1, d, CubicalSpaceCompressed< D, M > >::Vector
-                Vector;
-        typedef typename ExteriorPower< 0, 0, CubicalSpaceCompressed< D, M > >::Vector
-                Scalar;
-
-        typedef std::vector< std::pair< KeyType, KeyType > > Edges;
-        typedef std::vector< std::array< KeyType, 5 > > Cubes;
-
-        //typedef MortonHalfCubeComplex < D, M > MHCC;
-        typedef YeeFieldHalfCubeComplex < D, M > MHCC;
-
-        template < int _D >
-                using HalfCube = MortonHalfCube< _D, M >;
-        typedef HalfCube< D > Cube;
-        typedef MortonHalfCubeComplexIterator< d, M > Iterator;
-        template < int _D >
-        using Container = typename MHCC::template Container< _D >;
-	using CubicalSpaceCompressed< D, M >::ms_map;
-	using CubicalSpaceCompressed< D, M >::m_tree;
-	using CubicalSpaceCompressed< D, M >::mc_map;
-
-	void integrate()
-	{
-		std::for_each(begin(ms_map), end(ms_map), [i = 0] (auto &p) mutable {
-									p
-								});
-	}
-
-
-};
-*/
 #endif
